ef9f6ec93eaf6d1606914d44a735a1b620e444cd
Vinicius Aguiar <vinicius12ex@hotmail.com>
2019-09-03 12:13:35 -0300

    Initial commit

---
+++ Assets/Arduino_Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class Arduino_Controller : MonoBehaviour
+{
+    private SerialPort arduinoSerialPort;
+    public string port = "COM3";
+    public int baudRate = 9600;
+
+    //Cereal nescau = Arduino
+    private void Awake()
+    {
+        try
+        {
+            arduinoSerialPort = new SerialPort(port, baudRate)
+            {
+                ReadTimeout = 100
+            };
+
+            arduinoSerialPort.Open();
+
+            StartCoroutine(ProcessData());
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Erro ao abrir a porta." + e);
+        }
+    }
+
+    private IEnumerator ProcessData()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            try
+            {
+                string result = arduinoSerialPort.ReadLine();
+                //Debug.Log("Leitura Arduino" + result);
+
+                string[] resultData = result.Split(';');
+
+                string firstButton = resultData[0];
+                Debug.Log("Primeiro botão" + firstButton);
+            }
+            catch (Exception e)
+            {
+                //Debug.LogError("Erro ao processar dados" + e);
+            }
+        }
+    }
+}
---
+++ Assets/Teste.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    public ArdJoystick.ArdController ardController;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (ardController.GetKeyDown(ArdJoystick.ArdKeyCode.BUTTON_A))
+        {
+            //Action for Button A
+        }
+    }
+}
